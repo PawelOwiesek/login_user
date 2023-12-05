10a6384a7ef0ba5cc0a570ced7f6692f406f4c1e
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-05 10:15:48 +0100

    add media queries

--- src/features/loginPage/styled.js
+++ src/features/loginPage/styled.js
@@ -6,33 +6,61 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   background: linear-gradient(45deg, #d54b4b, #172470, #1edc14);
-  width: 800px;
-  height: 600px;
+  max-width: 800px;
+  max-height: 600px;
   border-radius: 20px;
   box-shadow: 30px -18px 100px 49px #2b5e47;
+
+  @media (max-width: 767px) {
+    font-size: 25px;
+    width: 80vw;
+    height: 90vh;
+  }
+
+  @media (max-width: 450px) {
+    font-size: 25px;
+    width: 90vw;
+  }
 `;
 
 export const Title = styled.h1`
   color: #f5f5f5;
   font-size: 42px;
+  @media (max-width: 767px) {
+    font-size: 25px;
+  }
 `;
+
 export const SubHeader = styled.h2`
   color: #f5f5f5;
   font-size: 36px;
+  @media (max-width: 767px) {
+    font-size: 25px;
+    margin: 0;
+  }
 `;
 export const InputLabel = styled.label`
   font-size: 30px;
   color: #fff;
   display: inline-block;
   margin: 20px;
+  @media (max-width: 767px) {
+    font-size: 20px;
+  }
 `;
 export const Input = styled.input`
   display: flex;
   font-size: 25px;
   margin-left: 40px;
   padding: 10px 25px;
-  width: 400px;
+  max-width: 400px;
   border-radius: 25px;
+  @media (max-width: 767px) {
+    font-size: 20px;
+    width: 150px;
+    height: 20px;
+    margin-left: 0;
+  }
 `;
 
 export const Button = styled.button`
@@ -43,4 +71,8 @@ export const Button = styled.button`
   padding: 10px;
   border-radius: 10px;
   margin: 10px auto;
+
+  @media (max-width: 767px) {
+    font-size: 16px;
+  }
 `;
