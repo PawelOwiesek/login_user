89016b60f47bcbdbc04717f1a8b36212a1bfb401
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-05 08:15:04 +0100

    add basic LoginForm layout

--- src/features/loginPage/LoginForm.js
+++ src/features/loginPage/LoginForm.js
@@ -1,11 +1,47 @@
 import { Container, Title } from "./styled.js";
+import { useState } from "react";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <Container>
       <header className="header">
         <Title>CV builder</Title>
       </header>
+      <main>
+        <section>
+          <form onSubmit={onFormSubmit}>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="your email address"
+                required
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                type="password"
+                name="password"
+                value={password}
+                placeholder="**********"
+                required
+              />
+            </label>
+            <button>Log In</button>
+          </form>
+          <button>No account yet? Register</button>
+        </section>
+      </main>
     </Container>
   );
 };
