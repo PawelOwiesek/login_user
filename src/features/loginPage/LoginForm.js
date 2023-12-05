357ab5e3b2b8c9a19f50c1b3960a2fd9aa8dff1e
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-05 07:57:23 +0100

    Initialized LoginForm component

---
+++ src/features/loginPage/LoginForm.js
@@ -0,0 +1,13 @@
+import { Container, Title } from "./styled.js";
+
+const LoginForm = () => {
+  return (
+    <Container>
+      <header className="header">
+        <Title>CV builder</Title>
+      </header>
+    </Container>
+  );
+};
+
+export default LoginForm;
