814e776896ef45f71f76397d494618552921c186
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-05 08:40:52 +0100

    created RegisterForm action with the given paramS

--- src/features/RegisterForm.js
+++ src/features/RegisterForm.js
@@ -1,5 +1,62 @@
-const RegisterForm = () => {
-  return <div>Register</div>;
+import { useState } from "react";
+import { Container, Title } from "./loginPage/styled";
+
+const RegisterForm = ({ formName }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Container>
+      <header className="header">
+        <Title>Register new account</Title>
+      </header>
+      <main>
+        <section>
+          <form onSubmit={onFormSubmit}>
+            <label>
+              Full name:
+              <input
+                type="text"
+                name="name"
+                value={name}
+                placeholder="your full name"
+                required
+              />
+            </label>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={email}
+                placeholder="your email address"
+                required
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                type="password"
+                name="password"
+                value={password}
+                placeholder="**********"
+                required
+              />
+            </label>
+            <button>Log In</button>
+          </form>
+          <button onClick={() => formName("login")}>
+            Already have an account? Log in
+          </button>
+        </section>
+      </main>
+    </Container>
+  );
 };
 
 export default RegisterForm;
