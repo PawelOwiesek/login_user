9a7905370f9f7319b3a610e75619f32f9842f6c0
Pawel Owiesek <Owiesek.p@gmail.com>
2023-12-05 08:40:06 +0100

    add toggleForm() and switch the componet interact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import RegisterForm from "./features/RegisterForm";
 import LoginForm from "./features/loginPage/LoginForm";
 
 function App() {
+  const [currentForm, setCurrentForm] = useState("login");
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
   return (
     <div>
-      <LoginForm />
+      {currentForm === "login" ? (
+        <LoginForm formName={toggleForm} />
+      ) : (
+        <RegisterForm formName={toggleForm} />
+      )}
     </div>
   );
 }
